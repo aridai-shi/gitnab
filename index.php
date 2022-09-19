0c8a2043ec13025ec5a93397be9e778edf1eb3ae
Jakub Kaczmarek <j.kaczmarek@s309.local>
2022-09-19 12:24:48 +0200

    Finished login page

--- index.php
+++ index.php
@@ -1,10 +1,35 @@
+<!DOCTYPE html>
 <?php
-// Initialize the session
-session_start();
- 
-// Check if the user is logged in, if not then redirect him to login page
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
-    header("location: login.php");
-    exit;
-}
+   include('verify_sesh.php');
 ?>
+
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>gitnab - Log in</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css">
+    <meta name="color-scheme" content="dark light">
+    <script>
+        if (window.matchMedia("(prefers-color-scheme: dark)").media === "not all") {
+            document.documentElement.style.display = "none";
+            document.head.insertAdjacentHTML(
+                "beforeend",
+                "<link id=\"css\" rel=\"stylesheet\" href=\"../dist/css/bootstrap.css\" onload=\"document.documentElement.style.display = ''\">"
+            );
+        }
+    </script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap.min.css" rel="stylesheet" media="(prefers-color-scheme: light)">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css" rel="stylesheet" media="(prefers-color-scheme: dark)">
+   <head>
+      <title>Welcome </title>
+   </head>
+   
+   <body>
+      <h1>Welcome <?php echo $_SESSION["login_user"]; ?></h1> 
+      <h2><a href = "logout.php">Sign Out</a></h2>
+   </body>
+   
+</html>
--- login.php
+++ login.php
@@ -2,95 +2,6 @@
 <html lang="en">
 
 <head>
-    <?php
-    // Initialize the session
-    session_start();
-
-    // Check if the user is already logged in, if yes then redirect him to welcome page
-    if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
-        header("location: index.php");
-        exit;
-    }
-
-    // Include config file
-    require_once "config.php";
-
-    // Define variables and initialize with empty values
-    $username = $password = "";
-    $username_err = $password_err = $login_err = "";
-
-    // Processing form data when form is submitted
-    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-        // Check if username is empty
-        if (empty(trim($_POST["username"]))) {
-            $username_err = "Please enter username.";
-        } else {
-            $username = trim($_POST["username"]);
-        }
-
-        // Check if password is empty
-        if (empty(trim($_POST["password"]))) {
-            $password_err = "Please enter your password.";
-        } else {
-            $password = trim($_POST["password"]);
-        }
-
-        // Validate credentials
-        if (empty($username_err) && empty($password_err)) {
-            // Prepare a select statement
-            $sql = "SELECT id, username, password FROM users WHERE username = ?";
-
-            if ($stmt = $mysqli->prepare($sql)) {
-                // Bind variables to the prepared statement as parameters
-                $stmt->bind_param("s", $param_username);
-
-                // Set parameters
-                $param_username = $username;
-
-                // Attempt to execute the prepared statement
-                if ($stmt->execute()) {
-                    // Store result
-                    $stmt->store_result();
-
-                    // Check if username exists, if yes then verify password
-                    if ($stmt->num_rows == 1) {
-                        // Bind result variables
-                        $stmt->bind_result($id, $username, $hashed_password);
-                        if ($stmt->fetch()) {
-                            if (password_verify($password, $hashed_password)) {
-                                // Password is correct, so start a new session
-                                session_start();
-
-                                // Store data in session variables
-                                $_SESSION["loggedin"] = true;
-                                $_SESSION["id"] = $id;
-                                $_SESSION["username"] = $username;
-
-                                // Redirect user to welcome page
-                                header("location: welcome.php");
-                            } else {
-                                // Password is not valid, display a generic error message
-                                $login_err = "Invalid username or password.";
-                            }
-                        }
-                    } else {
-                        // Username doesn't exist, display a generic error message
-                        $login_err = "Invalid username or password.";
-                    }
-                } else {
-                    echo "Oops! Something went wrong. Please try again later.";
-                }
-
-                // Close statement
-                $stmt->close();
-            }
-        }
-
-        // Close connection
-        $mysqli->close();
-    }
-    ?>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -106,8 +17,34 @@
             );
         }
     </script>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap.min.css" rel="stylesheet" media="(prefers-color-scheme: light)">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap-dark-5@1.1.3/dist/css/bootstrap-night.min.css" rel="stylesheet" media="(prefers-color-scheme: dark)">
+    <?php
+    include("config.php");
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
+    session_start();
+    $error = "";
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        // username and password sent from form 
+        $myusername = mysqli_real_escape_string($db, $_POST['login']);
+        $mypassword = mysqli_real_escape_string($db, $_POST['pass']);
+
+        $sql = "SELECT id FROM users WHERE login = '$myusername' and pass = '$mypassword'";
+        $result = mysqli_query($db, $sql);
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+        $count = mysqli_num_rows($result);
+
+        // If result matched $myusername and $mypassword, table row must be 1 row
+        if ($count == 1) {
+            $_SESSION['login_user'] = $myusername;
+            $_SESSION["loggedin"] = TRUE;
+            header("location: index.php");
+        } else {
+            $error = "Your login or password is invalid";
+        }
+    }
+    ?>
 </head>
 
 <body>
@@ -121,10 +58,19 @@
                 <div class="form-group">
                     <h2>Login</h2>
                     <input type="text" class="form-control  my-2" name="login" placeholder="Username" id="login" required>
-                    <input type="text" name="pass" class="form-control  my-2" placeholder="Password" id="pass" required>
+                    <input type="password" name="pass" class="form-control  my-2" placeholder="Password" id="pass" required>
                     <button type="submit" class="btn btn-success mb-2  my-2">Log in</button>
                 </div>
+                <?php
+            if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+                echo "<p class='text-danger'>You are already logged in, " . $_SESSION['login_user'] . "</p>";
+            }
+            if (!empty($error)) {
+                echo '<div class="text-danger">' . $error . '</div>';
+            }
+            ?>
             </form>
+            
         </div>
     </div>
 
